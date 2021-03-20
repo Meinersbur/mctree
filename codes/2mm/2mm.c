4535e8fc95794e7939278a4ffd541df4c9e6d1d4
Michael Kruse <mctree@meinersbur.de>
2021-03-20 15:52:10 -0500

    Manually distribute 2mm

--- codes/2mm/2mm.c
+++ codes/2mm/2mm.c
@@ -76,21 +76,24 @@ void kernel_2mm(int ni, int nj, int nk, int nl,
   int i, j, k;
 
 #pragma scop
+  for (i = 0; i < _PB_NI; i++)
+    for (j = 0; j < _PB_NJ; j++)
+	tmp[i][j] = SCALAR_VAL(0.0);
+
   /* D := alpha*A*B*C + beta*D */
   for (i = 0; i < _PB_NI; i++)
     for (j = 0; j < _PB_NJ; j++)
-      {
-	tmp[i][j] = SCALAR_VAL(0.0);
 	for (k = 0; k < _PB_NK; ++k)
 	  tmp[i][j] += alpha * A[i][k] * B[k][j];
-      }
+      
   for (i = 0; i < _PB_NI; i++)
     for (j = 0; j < _PB_NL; j++)
-      {
 	D[i][j] *= beta;
+
+  for (i = 0; i < _PB_NI; i++)
+    for (j = 0; j < _PB_NL; j++)
 	for (k = 0; k < _PB_NJ; ++k)
 	  D[i][j] += tmp[i][k] * C[k][j];
-      }
 #pragma endscop
 
 }
