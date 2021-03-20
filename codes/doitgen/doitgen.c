bd28def4a40ac99d8d847073e9902542b13f4ccf
Michael Kruse <mctree@meinersbur.de>
2021-03-20 16:26:41 -0500

    Manually distribute doitgen

--- codes/doitgen/doitgen.c
+++ codes/doitgen/doitgen.c
@@ -63,15 +63,20 @@ void kernel_doitgen(int nr, int nq, int np,
 
 #pragma scop
   for (r = 0; r < _PB_NR; r++)
-    for (q = 0; q < _PB_NQ; q++)  {
-      for (p = 0; p < _PB_NP; p++)  {
+    for (q = 0; q < _PB_NQ; q++)
+      for (p = 0; p < _PB_NP; p++)
 	sum[p] = SCALAR_VAL(0.0);
+
+  for (r = 0; r < _PB_NR; r++)
+    for (q = 0; q < _PB_NQ; q++)  
+      for (p = 0; p < _PB_NP; p++)  
 	for (s = 0; s < _PB_NP; s++)
 	  sum[p] += A[r][q][s] * C4[s][p];
-      }
+      
+  for (r = 0; r < _PB_NR; r++)
+    for (q = 0; q < _PB_NQ; q++)
       for (p = 0; p < _PB_NP; p++)
 	A[r][q][p] = sum[p];
-    }
 #pragma endscop
 
 }
