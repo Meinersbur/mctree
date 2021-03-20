44bd88ce651493446074edd73dea7dcc900fe149
Michael Kruse <mctree@meinersbur.de>
2021-03-20 15:34:43 -0500

    Manually distribute syrk

--- codes/syrk/syrk.c
+++ codes/syrk/syrk.c
@@ -71,14 +71,14 @@ void kernel_syrk(int n, int m,
 //A is NxM
 //C is NxN
 #pragma scop
-  for (i = 0; i < _PB_N; i++) {
+  for (i = 0; i < _PB_N; i++) 
     for (j = 0; j <= i; j++)
       C[i][j] *= beta;
-    for (k = 0; k < _PB_M; k++) {
+
+  for (i = 0; i < _PB_N; i++) 
+    for (k = 0; k < _PB_M; k++) 
       for (j = 0; j <= i; j++)
         C[i][j] += alpha * A[i][k] * A[j][k];
-    }
-  }
 #pragma endscop
 
 }
