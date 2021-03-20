fe71926fcac8b5b9d2cc32a9d28eacb560eebd38
Michael Kruse <mctree@meinersbur.de>
2021-03-20 15:21:43 -0500

    Manually distribute gesummv

--- codes/gesummv/gesummv.c
+++ codes/gesummv/gesummv.c
@@ -72,16 +72,21 @@ void kernel_gesummv(int n,
 
 #pragma scop
   for (i = 0; i < _PB_N; i++)
-    {
       tmp[i] = SCALAR_VAL(0.0);
+
+  for (i = 0; i < _PB_N; i++)
       y[i] = SCALAR_VAL(0.0);
-      for (j = 0; j < _PB_N; j++)
-	{
+
+  for (i = 0; i < _PB_N; i++) 
+      for (j = 0; j < _PB_N; j++) 
 	  tmp[i] = A[i][j] * x[j] + tmp[i];
+
+  for (i = 0; i < _PB_N; i++)
+      for (j = 0; j < _PB_N; j++) 
 	  y[i] = B[i][j] * x[j] + y[i];
-	}
-      y[i] = alpha * tmp[i] + beta * y[i];
-    }
+
+  for (i = 0; i < _PB_N; i++)
+        y[i] = alpha * tmp[i] + beta * y[i];
 #pragma endscop
 
 }
