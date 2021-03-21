52d7713dc531f2aecf37557049cf77a9117c4ec7
Michael Kruse <mctree@meinersbur.de>
2021-03-20 21:29:15 -0500

    CoMD compile&run

---
+++ codes/comd/compile_run.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env bash
+set -e
+
+SCRIPTPATH=`realpath --no-symlinks $(dirname $0)`
+BASENAME=`basename "${SCRIPTPATH}"`
+ROOTPATH=`realpath --no-symlinks "${SCRIPTPATH}/../.."`
+
+if [[ -z "${CLANG_PREFIX}" ]]; then
+  echo "CLANG_PREFIX environment variable not set"
+  exit 1
+fi  
+
+"${CLANG_PREFIX}/bin/clang" -I"${CLANG_PREFIX}/projects/openmp/runtime/src" -I"${CLANG_PREFIX}/runtimes/runtimes-bins/openmp/runtime/src" -L"${CLANG_PREFIX}/runtimes/runtimes-bins/openmp/runtime/src" \
+  -mllvm -polly-position=early -O3 -march=native \
+  *.c -o "${BASENAME}" \
+  -I"${SCRIPTPATH}" -DDOUBLE -lm \
+
+"${SCRIPTPATH}/${BASENAME}"
+
+
