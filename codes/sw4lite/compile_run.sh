9068890b2bf85081194c2529fb2d9629537de6e2
Michael Kruse <mctree@meinersbur.de>
2021-03-22 23:13:56 -0500

    [sw4lite] Don't always compile everytging in compile_run script

--- codes/sw4lite/compile_run.sh
+++ codes/sw4lite/compile_run.sh
@@ -8,12 +8,16 @@ ROOTPATH=`realpath --no-symlinks "${SCRIPTPATH}/../.."`
 if [[ -z "${CLANG_PREFIX}" ]]; then
   echo "CLANG_PREFIX environment variable not set"
   exit 1
-fi  
+fi
 
-"${CLANG_PREFIX}/bin/clang++" -I"${CLANG_PREFIX}/projects/openmp/runtime/src" -I"${CLANG_PREFIX}/runtimes/runtimes-bins/openmp/runtime/src" -L"${CLANG_PREFIX}/runtimes/runtimes-bins/openmp/runtime/src" \
+CFLAGS=(-I"${CLANG_PREFIX}/projects/openmp/runtime/src" -I"${CLANG_PREFIX}/runtimes/runtimes-bins/openmp/runtime/src" -L"${CLANG_PREFIX}/runtimes/runtimes-bins/openmp/runtime/src" \
   -mllvm -polly-position=early -O3 -march=native \
-  *.C -o "${BASENAME}" \
   -I"/usr/include/x86_64-linux-gnu/mpich" -lmpich -llapack -lm \
-  -I"${SCRIPTPATH}" -DSW4_CROUTINES -DNDEBUG -mllvm -polly -mllvm -polly-position=early -mllvm -polly-only-func=ljForce_kernel -mllvm -polly-allow-nonaffine -mllvm -polly-process-unprofitable -mllvm -polly-allow-nonaffine-branches -mllvm -polly-print-instructions -mllvm -polly-use-llvm-names
+  -I"${SCRIPTPATH}" -DSW4_CROUTINES -DNDEBUG -mllvm -polly -mllvm -polly-position=early -mllvm -polly-only-func=ljForce_kernel -mllvm -polly-allow-nonaffine -mllvm -polly-process-unprofitable -mllvm -polly-allow-nonaffine-branches -mllvm -polly-print-instructions -mllvm -polly-use-llvm-names)
+
+#"${CLANG_PREFIX}/bin/clang++" ${CFLAGS[*]} -c *.C
+rm -f EW.o
+
+"${CLANG_PREFIX}/bin/clang++" ${CFLAGS[*]}  EW.C *.o  -o "${BASENAME}" \
 
 "${SCRIPTPATH}/${BASENAME}" LOH.1-h100.in
